56ef2bb5f99b4c72dacdc5933b055528dc06bd91
MaryBerry188 <masha-188@mail.ru>
2020-10-19 19:17:20 +0300

    добавление карточки

--- js/main.js
+++ js/main.js
@@ -57,7 +57,9 @@ const getRandomRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-document.querySelector(`.map`).classList.remove(`map--faded`);
+const map = document.querySelector(`.map`);
+map.classList.remove(`map--faded`);
+
 
 const getRandomElement = function (array) {
   let randomStr;
@@ -122,3 +124,51 @@ for (let i = 0; i < pinsBase.length; i++) {
   mapFragment.appendChild(renderElement(pinsBase[i]));
   mapList.appendChild(mapFragment);
 }
+
+const offerCard = document.querySelector('#card').content.querySelector('.map__card');
+
+const getOfferCard = function (element) {
+  const card = offerCard.cloneNode(true);
+  card.querySelector('.popup__avatar').src = element.author.avatar;
+  card.querySelector('.popup__title').textContent = element.offer.title;
+  card.querySelector('.popup__text--address').textContent = element.offer.address;
+  card.querySelector('.popup__text--price').textContent = `${element.offer.price} ₽/ночь`;
+  card.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} комнаты для ${element.offer.guests}`;
+  card.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
+  card.querySelector('.popup__type').textContent = element.offer.type;
+  const featuresList = card.querySelector('.popup__features');
+  featuresList.innerHTML = '';
+
+  if (element.offer.features.length === 0) {
+    card.removeChild(featuresList);
+  }
+
+  for (let i = 0; i < element.offer.features.length; i++) {
+    const featuresItem = document.createElement('li');
+    featuresItem.classList.add('.popup__feature', `popup__feature--${element.offer.features[i]}`);
+    featuresList.appendChild(featuresItem);
+  }
+
+  card.querySelector('.popup__description').textContent = element.offer.description;
+  const photoList = card.querySelector('.popup__photos');
+  const photoItem = photoList.querySelector('.popup__photo');
+
+  if (element.offer.photos.length === 0) {
+    card.removeChild(photoList);
+  }
+
+  for (let j = 0; j < element.offer.photos.length; j++) {
+    const item = photoItem.cloneNode(true);
+    item.src = element.offer.photos[j];
+    photoList.appendChild(item);
+  }
+  photoList.removeChild(photoItem);
+
+  return card;
+};
+
+const mapFilter = map.querySelector('.map__filters-container');
+const pinPopupFragment = document.createDocumentFragment();
+const pinPopup = getOfferCard(pinsBase[0]);
+map.insertBefore(pinPopupFragment.appendChild(pinPopup), mapFilter);
+
